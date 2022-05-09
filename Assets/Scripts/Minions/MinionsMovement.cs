694f114043834572ed83c54b3f4188a423c4c371
Loann <courcoul66@gmail.com>
2022-05-09 13:54:58 +0200

    Start reworking movement player

--- Assets/Scripts/Minions/MinionsMovement.cs
+++ Assets/Scripts/Minions/MinionsMovement.cs
@@ -8,7 +8,19 @@ public class MinionsMovement : MonoBehaviour
 {
     public MinionsScriptableObject playerStats;
     
-    private NavMeshAgent _agent;
+    public float angle;
+    public float rayRange;
+    public Transform minionSprite;
+    public float angleRaycast;
+    
+    public int numberOfRays ;
+    public  float totalAngle;
+
+    public LayerMask layerPlayer;
+    
+    private float delta; 
+    private Vector3 pos;
+
     private float speed;
     private Camera _cam;
 
@@ -17,14 +29,13 @@ public class MinionsMovement : MonoBehaviour
         _cam = Camera.main;
         speed = playerStats.speed;
     }
-
+    public Texture2D cursorTexture;
+  
     // Start is called before the first frame update
     void Start()
     {
-        _agent = GetComponent<NavMeshAgent>();
-        _agent.updateRotation = false;
-        _agent.updateUpAxis = false;
-        _agent.speed = speed;
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+       
     }
 
     // Update is called once per frame
@@ -32,15 +43,58 @@ public class MinionsMovement : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            var ray = _cam.ScreenPointToRay(Input.mousePosition);
-            
-            Vector3 diff = _cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            Vector3 diff = _cam.ScreenToWorldPoint(Input.mousePosition) - minionSprite.position;
             diff.Normalize();
  
             float rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+            minionSprite.rotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+           
+            var t = transform;
             
-            _agent.SetDestination(ray.origin);
+            delta = totalAngle / numberOfRays;
+            pos = transform.position;
+            
+            for (int i = 0; i < numberOfRays; i++)
+            {
+                var dir = Quaternion.Euler(0, 0, i * delta) * transform.right;               
+                Debug.DrawRay(pos, dir * rayRange, Color.green);
+                
+                RaycastHit2D hit = Physics2D.Raycast(pos, dir* rayRange, layerPlayer);
+                
+                if (hit.collider != null && hit.collider.name != gameObject.name)
+                {
+                    speed = 0;
+                }
+                else
+                {
+                    speed = playerStats.speed;
+                    Vector3 mousePosition = _cam.ScreenToWorldPoint(Input.mousePosition);
+                    transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+                }
+            }
+            
         }
     }
+    
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        delta = totalAngle / numberOfRays;
+        pos = transform.position;
+            
+        for (int i = 0; i < numberOfRays; i++)
+        {
+            var dir = Quaternion.Euler(0, 0, i * delta) * transform.right;            
+            Debug.DrawRay(pos, dir * rayRange, Color.green);
+
+            RaycastHit2D hit = Physics2D.Raycast(pos, dir* rayRange, layerPlayer);
+            
+            if (hit.collider != null && hit.collider.name != gameObject.name)
+            {
+                Debug.DrawRay(pos, dir * rayRange, Color.red);
+            }
+        }   
+    }
+#endif
+   
 }
